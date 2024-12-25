f3518dda3ae887ebad38bfc6d22c1c2c0c49e1b9
LucasJSch <lucas.scheinkerman@gmail.com>
2024-12-25 15:19:47 -0300

    Kafka publishing ok.

---
+++ optriment_pipeline/include/Serializable.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace optriment {
+class Serializable 
+{
+public:
+  virtual char* toBytes() const = 0;
+  virtual void fromBytes(char* ptr, size_t len) = 0;
+};
+} // namespace optriment
--- optriment_pipeline/src/optriment_pipeline.cpp
+++ optriment_pipeline/src/optriment_pipeline.cpp
@@ -1,8 +1,11 @@
+// TODO: Delete unnecessary includes and reorganize according to style guide.
 #include <gst/gst.h>
 #include <glib.h>
 
 #include "Pipeline.h"
 #include "ArgParser.h"
+#include "KafkaPublisher.h"
+#include "RegionCountMessage.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +17,7 @@
 
 #include "gstnvdsmeta.h"
 
+#include <librdkafka/rdkafkacpp.h>
 
 #define RETURN_ON_PARSER_ERROR(parse_expr) \
   if (NVDS_YAML_PARSER_SUCCESS != parse_expr) { \
@@ -29,6 +33,37 @@ void initializer(int argc, char *argv[])
     struct cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, current_device);
 }
+
+static GstPadProbeReturn probe (GstPad * pad, GstPadProbeInfo * info, gpointer u_data, optriment::KafkaPublisher& publisher)
+{
+    NvDsObjectMeta *obj_meta = NULL;
+        NvDsMetaList * l_frame = NULL;
+        NvDsMetaList * l_obj = NULL;
+    
+        GstBuffer* buffer = gst_pad_probe_info_get_buffer(info);
+        NvDsBatchMeta* batch_meta = gst_buffer_get_nvds_batch_meta(buffer);
+
+        std::shared_ptr<optriment::RegionCountMessage> message = std::make_shared<optriment::RegionCountMessage>();
+        for (l_frame = batch_meta->frame_meta_list; l_frame != NULL; l_frame = l_frame->next) {
+            NvDsFrameMeta *frame_meta = (NvDsFrameMeta *) (l_frame->data);
+            std::stringstream out_string;
+            for (NvDsMetaList * l_user = frame_meta->frame_user_meta_list; l_user != NULL; l_user = l_user->next) {
+                NvDsUserMeta *user_meta = (NvDsUserMeta *) l_user->data;
+                if (user_meta->base_meta.meta_type != NVDS_USER_FRAME_META_NVDSANALYTICS)
+                    continue;
+
+                NvDsAnalyticsFrameMeta *meta =
+                    (NvDsAnalyticsFrameMeta *) user_meta->user_meta_data;
+                for (std::pair<std::string, uint32_t> status : meta->objInROIcnt){
+                    message->add(status.first, status.second);
+                }
+                publisher.publish(message);
+            }
+        }
+        return GST_PAD_PROBE_OK;
+}
+
+static optriment::KafkaPublisher kafka_publisher("localhost:9092", "topic1");
 
 int main (int argc, char *argv[])
 {
@@ -62,24 +97,15 @@ int main (int argc, char *argv[])
     .addElement("nvinfer", "pgie")
     .addElement("nvvideoconvert", "nvvideo-converter")
     .addElement("nvdsosd", "on-screen-display")
-    // Tee to stream video and Kafka messages in parallel
-    .addElement("tee", "broker-video-tee")
-    .addElement("queue", "broker-tee")
-    .addElement("nvmsgbroker", "msg-broker")
-    .addElement("queue", "video-tee")
-    .addElement("nvmsgconv", "msg-converter")
     .addElement("nveglglessink", "video-renderer");
 
     RETURN_ON_PARSER_ERROR(nvds_parse_gie_type(&pgie_type, file_path,
         "primary-gie"));
-
     RETURN_ON_PARSER_ERROR(nvds_parse_file_source(pipeline.getElement("file-source"), file_path, "source"));
     RETURN_ON_PARSER_ERROR(nvds_parse_streammux(pipeline.getElement("stream-muxer"), file_path, "streammux"));
     RETURN_ON_PARSER_ERROR(nvds_parse_gie(pipeline.getElement("pgie"), file_path, "primary-gie"));
     RETURN_ON_PARSER_ERROR(nvds_parse_tracker(pipeline.getElement("tracker"), file_path, "tracker"));
 
-    RETURN_ON_PARSER_ERROR(nvds_parse_msgconv (pipeline.getElement("msg-converter"), file_path, "msgconv"));
-    RETURN_ON_PARSER_ERROR(nvds_parse_msgbroker(pipeline.getElement("msg-broker"), file_path, "msgbroker"));
 
     if (!pipeline.getPipeline()) {
         g_printerr ("One element could not be created. Exiting.\n");
@@ -92,38 +118,13 @@ int main (int argc, char *argv[])
     pipeline.linkElements({"file-source", "h264-parser", "nvv4l2-decoder"});
     pipeline.linkElements({"stream-muxer","pgie", "tracker", "analytics1",
                            "analytics2", "nvvideo-converter",
-                           "on-screen-display", "broker-video-tee"});
-    pipeline.linkElements({"broker-tee", "msg-converter", "msg-broker"});
-    pipeline.linkElements({"video-tee", "video-renderer"});
-
-    pipeline.linkWithRequestPadTee("broker-tee", "broker-video-tee");
-    pipeline.linkWithRequestPadTee("video-tee", "broker-video-tee");
+                           "on-screen-display", "video-renderer"});
 
     pipeline.attachProbe([](GstPad* pad, GstPadProbeInfo* info, gpointer u_data) {
-        GstBuffer* buffer = gst_pad_probe_info_get_buffer(info);
-        NvDsBatchMeta* batch_meta = gst_buffer_get_nvds_batch_meta(buffer);
-        g_printerr("scheink");
-        if (batch_meta == nullptr) {
-            return GST_PAD_PROBE_OK;
-        }
-        for (NvDsMetaList* l_frame = batch_meta->frame_meta_list; l_frame != nullptr; l_frame = l_frame->next) {
-            NvDsFrameMeta* frame_meta = (NvDsFrameMeta*)l_frame->data;
-            if (frame_meta == nullptr) {
-                continue;
-            }
-            for (NvDsMetaList* l_obj = frame_meta->obj_meta_list; l_obj != nullptr; l_obj = l_obj->next) {
-                NvDsObjectMeta* obj_meta = (NvDsObjectMeta*)l_obj->data;
-                if (obj_meta == nullptr) {
-                    continue;
-                }
-                g_print("Object: %s\n", obj_meta->obj_label);
-            }
-        }
-        return GST_PAD_PROBE_OK;
-    }, "on-screen-display", "sink");
+        return probe(pad, info, u_data, kafka_publisher);
+    }, "analytics2", "src");
 
     // TODO: Move to run().
-    gst_element_set_state(pipeline.getPipeline(), GST_STATE_PLAYING);
     pipeline.run();
     return 0;
 }
